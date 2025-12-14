9f4179da7ac578cd2e4eea1042b142457b71afd1
Subham Prasad Achary <acharysubham2803@gmail.com>
2025-12-14 23:53:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,25 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name)) {
+            return `images/[name]-[hash][extname]`
+          }
+          return `${ext}/[name]-[hash][extname]`
+        }
+      }
+    }
+  },
+  publicDir: 'public',
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  }
 })
